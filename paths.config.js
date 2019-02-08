31e26bcba3ee7f16d3a1750e3af476c5445e6a75
Shachar Leuchter <shachar.leuchter@jvm.de>
2019-02-08 14:53:04 +0100

    feat(path-config): new project paths config

---
+++ paths.config.js
@@ -0,0 +1,8 @@
+// Export projects path alias
+module.exports = {
+    'build': `${process.env.PROJECT_CWD}/build`,
+    'dev': `${process.env.PROJECT_CWD}/develop`,
+    'release': `${process.env.PROJECT_CWD}/release`,
+    'webui': `${process.env.PROJECT_CWD}/web-ui`,
+    'docs': `${process.env.PROJECT_CWD}/docs`
+};
