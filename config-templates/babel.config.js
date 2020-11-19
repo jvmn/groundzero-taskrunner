23010eab772de26d2c32486d7362737e9569035d
Oliver Müller <oliver.mueller@jvm.de>
2020-11-19 17:50:30 +0100

    feat(webpack): migration + updates

--- config-templates/babel.config.js
+++ config-templates/babel.config.js
@@ -3,18 +3,7 @@ module.exports = function (api) {
     api.cache(true);
 
     const presets = [
-        [process.env.PWD + "/node_modules/@babel/preset-env", {
-            // modules: false,
-            targets: {
-                "browsers": [
-                    "defaults", //(> 0.5%, last 2 versions, Firefox ESR, not dead)
-                    "ie 10",
-                    "ie 11"
-                ]
-            },
-            // for uglifyjs...
-            forceAllTransforms: process.env === "production"
-        }],
+        [process.env.PWD + "/node_modules/@babel/preset-env"],
     ];
     const plugins = [];
 
